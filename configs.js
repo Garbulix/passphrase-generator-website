5b2b9ca0be8452fc528c83822b96b31bd6b59784
Bartłomiej Garbiec <garbiec@bartek.me>
2021-04-09 20:03:37 +0200

    add exports, merge webpageElements with configs

--- configs.js
+++ configs.js
@@ -1,11 +1,26 @@
-const availableWordlists = [english, englishUnique, polish];
-const defaultWordlistIndex = 0;
+export const availableWordlists = [english, englishUnique, polish];
+export const defaultWordlistIndex = 0;
 
-const passphraseDefaults = {
+export const passphraseDefaults = {
     wordlist: availableWordlists[defaultWordlistIndex].words,
     wordCount: 4,
     capitalized: false,
     insertNumber: false,
     maxWordCount: 12,
     minWordCount: 3,
+};
+
+/** All input related elements (checkboxes, buttons, select lists and labels for them) */
+export const inputs = {
+    wordlist: document.querySelector("#select-wordlist"),
+    wordCount: document.querySelector("#word-count"),
+    wordCountLabel: document.querySelector("#word-count-label"),
+    capitalized: document.querySelector("#if-capitalize"),
+    insertNumber: document.querySelector("#if-insert-number"),
+    newPassBtn: document.querySelector("#generate-btn"),
+};
+
+/** outputs - for the moment there is only one */
+export const outputs = {
+    passphrase: document.querySelector("#pass-output"),
 };
